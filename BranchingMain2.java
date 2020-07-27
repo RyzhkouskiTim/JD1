111596ea87080329d03d66fb40134d2a1304a0c7
unknown <50100gg@gmail.com>
2020-07-27 15:06:33 +0300

    second commit

--- BranchingMain2.java
+++ BranchingMain2.java
@@ -4,7 +4,7 @@ package branching;
 public class BranchingMain2 {
     public static void main(String[] args) {
         int a = 99;
-        int b = a == 99 ? 7 : 56;
+        int b = a == 99 ? 7 : 5678;
         System.out.println(b);
     }
 }
